6c32e036b7daf66de8f70faeade3bf758aae1ab8
ishanRox <roxishan8@gmail.com>
2022-04-15 20:48:55 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  //React useState and setState don’t make changes directly to the state object; they create queues to optimize //performance, which is why the changes don’t update immediately.
+
   const [counter, setCounter] = useState(0);
+  const increaseCount = () => {
+    setCounter(counter + 1);
+  };
 
   return (
     <div>
       <h1>Hello React Hooks !</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>counter value : {counter}</p>
+      <button onClick={increaseCount}>increase count </button>
     </div>
   );
 }
