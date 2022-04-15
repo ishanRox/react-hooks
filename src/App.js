76d51a6bdaf0b600f4c73f434672167a9be67fa5
ishanRox <roxishan8@gmail.com>
2022-04-15 22:54:21 +0530

    useEffect hook added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import axios from 'axios';
 
 export default function App() {
   //https://reactjs.org/docs/hooks-reference.html
-  
+
   //useState
   //React useState and setState don’t make changes directly to the state object; they create queues to optimize //performance, which is why the changes don’t update immediately.
 
@@ -14,20 +14,28 @@ export default function App() {
   };
 
   //useEffect
+  const [data, setData] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     axios
-    .get("https://jsonplaceholder.typicode.com/comments")
-    .then((response) => {
-      setData(response.data[0].email);
-      console.log("API WAS CALLED");
-    });
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .then((response) => {
+        setData(response.data[0].email);
+        console.log('API WAS CALLED');
+      });
   });
   return (
     <div>
       <h1>Hello React Hooks !</h1>
+
+      <h2>useState</h2>
       <p>counter value : {counter}</p>
       <button onClick={increaseCount}>increase count </button>
+      <hr />
+
+      <h2>useEffect</h2>
+      <p>data fetched by axios request : {data}</p>
+      <hr />
     </div>
   );
 }
