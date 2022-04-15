a38440418f429422341753107884084c0ac2ce55
ishanRox <roxishan8@gmail.com>
2022-04-15 22:44:55 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import './style.css';
+import axios from 'axios';
 
 export default function App() {
+  //https://reactjs.org/docs/hooks-reference.html
+  
+  //useState
   //React useState and setState don’t make changes directly to the state object; they create queues to optimize //performance, which is why the changes don’t update immediately.
 
   const [counter, setCounter] = useState(0);
@@ -9,6 +13,16 @@ export default function App() {
     setCounter(counter + 1);
   };
 
+  //useEffect
+
+  useEffect(()=>{
+    axios
+    .get("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => {
+      setData(response.data[0].email);
+      console.log("API WAS CALLED");
+    });
+  });
   return (
     <div>
       <h1>Hello React Hooks !</h1>
