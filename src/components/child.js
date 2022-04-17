9aa16cabc72436ac6a190fb44c1222d317d9f29b
Drone <roxishan8@gmail.com>
2022-04-17 19:29:43 +0530

    forwardRef info added

--- src/components/child.js
+++ src/components/child.js
@@ -6,6 +6,9 @@ import React, {
 
 //we used forwardRef and useImperativeHandle to make this referable component
 //see out toggle state in child not in the parent we havent uplifted the state
+//In React we can enable  refs passing to other components using a function called forwardRef
+// forwardRef is a function used to pass the ref to a child component.
+//useImperativeHandle gives you the ability to modify a ref that has been created.
 export default forwardRef(function Child(props, ref) {
     const [toggle, setToggle] = useState(false);
 
